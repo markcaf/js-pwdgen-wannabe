bf9840f77c21be63c5e81529cdacbac455515011
markcaf <marcocafassog@gmail.com>
2022-06-13 16:32:36 +0200

    Added code's comments

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+// Richiesta dei dati all'utente
 const userName = prompt("Inserisci il tuo nome");
 const userLastName = prompt("Inserisci il tuo cognome");
 const userFavoriteColor = prompt("Inserisci il tuo colore preferito");
 console.log(userName, userLastName, userFavoriteColor);
+// Scrittura a schermo della password generata
 document.getElementById("user-password").innerHTML = userName + userLastName + userFavoriteColor + "22";
